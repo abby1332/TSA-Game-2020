b082dc6574223eb7f4591aad2b076f619da6785e
abby1332 <67126411+abby1332@users.noreply.github.com>
2023-12-18 15:01:41 -0500

    Add files via upload
    Game files for the 4 games that make up the main game

---
+++ Olympics 2020/run.bat
@@ -0,0 +1,6 @@
+@echo off
+START /wait IntroAnimation\OlympicsMenu.exe
+START /wait skeetshoot\SkeetShoot.exe
+START /wait hurdles\hurdles.exe
+START /wait Rowing\RowingGame.exe
+START /wait Gameover\gameover.exe
